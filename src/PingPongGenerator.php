b0c8e79df1dfa3a80b40ad5060ef9d3c2bbf8459
Ian McKenney & Mike Chastain <student@epicodus.com>
2015-08-10 16:10:45 -0700

    implemented final spec

--- src/PingPongGenerator.php
+++ src/PingPongGenerator.php
@@ -26,6 +26,49 @@
                 }
             }//end while
             return $ping_pong_array;
-        }
+        }//end function
+
+
+        function generatePongArray($input){
+            $ping_pong_array = array();
+            $i = 1;
+            while($i <= $input) {
+                if(($i % 5) == 0){
+                    array_push($ping_pong_array, "pong");
+                    ++$i;
+                }else{
+                    array_push($ping_pong_array, $i);
+                    ++$i;
+                }
+            }//end while
+            return $ping_pong_array;
+        }//end function
+
+        function generatePingPongArray($input)
+        {
+            $ping_pong_array = array();
+            $i = 1;
+            while($i <= $input) {
+
+            if(($i % 5 == 0) && ($i % 3 == 0))
+            {
+                array_push($ping_pong_array, "ping-pong");
+            }
+            elseif($i % 5 == 0)
+            {
+                array_push($ping_pong_array, "pong");
+            }
+            elseif($i % 3 == 0)
+                {
+                    array_push($ping_pong_array, "ping");
+                }
+            else{
+                array_push($ping_pong_array, $i);
+                }
+            ++$i;
+            }//end while
+            return $ping_pong_array;
+        }//end function
+
     }
 ?>
--- tests/PingPongGeneratorTest.php
+++ tests/PingPongGeneratorTest.php
@@ -7,7 +7,7 @@
 //1. Print out numbers in order until the number the user enters is printed.
 
 //Input: 21
-//Output: 1,2,3,4,...,21
+//Output: 1,2,3,4,...,11
 
         function test_printNumbersUpToInput()
         {
@@ -38,8 +38,45 @@
 
             //Assert
             $this->assertEquals(array(1,2,"ping",4,5,"ping",7,8,"ping",10,11), $result);
-        }
+        }//end function
 
+//3. Given a user input of an integer, should return an array of integers except where the number is divisible by 5
+//in that case it should be a string "pong"
+
+//Input:  11
+//Output: array(1,2,3,4,"pong",6,7,8,9,"pong",11)
+        function test_generatePongArray()
+        {
+            //Arrange
+            $test_PingPongGenerator = new PingPongGenerator;
+            $input = 11;
+
+            //Act
+            $result = $test_PingPongGenerator->generatePongArray($input);
+
+            //Assert
+            $this->assertEquals(array(1,2,3,4,"pong",6,7,8,9,"pong",11), $result);
+
+        }//end function
+
+
+//4. Given a user input of an integer, should count up and push to an array.
+//If the value is divisible by 3 should push "ping", if the value is 5, should push "pong".
+//If the value is divisible by BOTH 5 and 3 push "ping-pong".  Return array.
+//input: 15
+//output: array(1,2,"ping",4,"pong","ping",7,8,"ping","pong",11,"ping",13,14,"ping-pong")
+
+    function test_generatePingPongArray()
+    {
+        $test_PingPongGenerator = new PingPongGenerator;
+        $input = 15;
+
+        //Act
+        $result = $test_PingPongGenerator->generatePingPongArray($input);
+
+        //Assert
+        $this->assertEquals(array(1,2,"ping",4,"pong","ping",7,8,"ping","pong",11,"ping",13,14,"ping-pong"), $result);
     }
+    }//end class
 
 ?>
